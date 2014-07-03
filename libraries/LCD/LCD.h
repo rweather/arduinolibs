e197d65702ebba079269d999c469a2d2ddbbbb3f
Angus Gratton <angus@freetronics.com>
2014-07-03 12:38:58 +1000

    LCD: Add capability to override LCD output pin assignments if pins were moved

--- libraries/LCD/LCD.h
+++ libraries/LCD/LCD.h
@@ -49,6 +49,9 @@ class LCD : public LiquidCrystal {
 public:
     LCD() : LiquidCrystal(8, 9, 4, 5, 6, 7) { init(); }
     LCD(uint8_t pin9) : LiquidCrystal(8, pin9, 4, 5, 6, 7) { init(); }
+    LCD(uint8_t rs, uint8_t enable,
+        uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3)
+      : LiquidCrystal(rs, enable, d0, d1, d2, d3) { init(); }
 
     uint8_t backlightPin() const { return _backlightPin; }
     void setBacklightPin(uint8_t pin);
