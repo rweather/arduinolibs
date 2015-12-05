13300f16d6bac1902f02d7e79cbb926064c5bc06
Rhys Weatherley <rhys.weatherley@gmail.com>
2015-12-05 17:51:39 +1000

    Update docs

--- search/classes_10.js
+++ search/classes_10.js
@@ -5,5 +5,6 @@ var searchData=
   ['sha3_5f256',['SHA3_256',['../classSHA3__256.html',1,'']]],
   ['sha3_5f512',['SHA3_512',['../classSHA3__512.html',1,'']]],
   ['sha512',['SHA512',['../classSHA512.html',1,'']]],
-  ['softi2c',['SoftI2C',['../classSoftI2C.html',1,'']]]
+  ['softi2c',['SoftI2C',['../classSoftI2C.html',1,'']]],
+  ['speck',['Speck',['../classSpeck.html',1,'']]]
 ];
